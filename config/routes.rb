039e2708383852bebc60118117fba548dc68024c
Sean C Davis <scdavis41@gmail.com>
2016-01-29 13:07:21 -0500

    base styles for intro card

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   get 'install/:step' => 'install#run', :as => :install
+  post 'install/:step/next' => 'install#next', :as => :install_next
 
   root :to => 'application#home'
 

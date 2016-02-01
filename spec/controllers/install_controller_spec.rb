8e8ad6e561a332960c2c263548cc5fe9e25637a3
Sean C Davis <scdavis41@gmail.com>
2016-02-01 14:53:19 -0500

    fix failing tests

--- spec/controllers/install_controller_spec.rb
+++ spec/controllers/install_controller_spec.rb
@@ -5,20 +5,20 @@ describe InstallController do
   before(:each) { remove_config }
   after(:each) { remove_config }
 
-  describe '#run' do
+  describe '#show' do
     it 'redirects no step to the current step' do
-      get :run, :step => nil
+      get :show, :step => nil
       expect(response).to redirect_to(install_path(1))
     end
     it 'redirects the wrong step to the current step' do
-      get :run, :step => 2
+      get :show, :step => 2
       expect(response).to redirect_to(install_path(1))
     end
   end
 
-  describe '#next' do
+  describe '#update' do
     it 'bumps the current_step and redirects' do
-      post :next, :step => 3 # step doesn't actually matter
+      post :update, :step => 3 # step doesn't actually matter
       expect(response).to redirect_to(install_path(2))
     end
   end

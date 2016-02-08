75a58fc031c8f22913709d26db1cd50c697d2dce
Sean C Davis <scdavis41@gmail.com>
2016-02-08 13:36:37 -0500

    add a minimal form builder (that doesn't add labels)

---
+++ app/helpers/form_helper.rb
@@ -0,0 +1,16 @@
+module FormHelper
+
+  def minimal_form_for(object, *args, &block)
+    options = args.extract_options!
+    simple_form_for(object,
+                    *(args << options.merge(:builder => MinimalFormBuilder)),
+                    &block)
+  end
+
+  def minimal_fields_for(*args, &block)
+    options = args.extract_options!
+    simple_fields_for(*(args << options.merge(:builder => MinimalFormBuilder)),
+                      &block)
+  end
+
+end

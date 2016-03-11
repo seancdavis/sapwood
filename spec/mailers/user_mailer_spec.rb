b1d1802ec7267947da145d3f62df1bf77be23ad6
Sean C Davis <scdavis41@gmail.com>
2016-03-11 15:02:46 -0500

    specs for the user mailer

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,5 +1,25 @@
 require "rails_helper"
 
-RSpec.describe UserMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe UserMailer, :type => :mailer do
+
+  describe '#welcome' do
+    before(:each) do
+      @user = create(:user)
+      @mail = UserMailer.welcome(@user)
+    end
+    it 'sends from the settings default' do
+      expect(@mail.from).to eq([Sapwood.config.default_from])
+    end
+    it 'sends to the user' do
+      expect(@mail.to).to eq([@user.email])
+    end
+    it 'renders a subject' do
+      expect(@mail.subject).to eq('Welcome to Sapwood!')
+    end
+    it 'has a link to start using Sapwood' do
+      path = "#{@user.id}/#{@user.sign_in_id}/#{@user.sign_in_key}"
+      expect(@mail.body.encoded).to include(path)
+    end
+  end
+
 end

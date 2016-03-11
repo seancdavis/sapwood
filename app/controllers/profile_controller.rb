bb4d8177cee1d3261941e004cc54ff18e096fc1c
Sean C Davis <scdavis41@gmail.com>
2016-03-11 14:07:25 -0500

    user can change their own profile

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -5,6 +5,9 @@ class ProfileController < ApplicationController
 
   def update
     if current_user.update(profile_params)
+      if changing_password?
+        sign_in(current_user, :bypass => true)
+      end
       redirect_to edit_profile_path, :notice => 'Profile updated successfully!'
     else
       render 'edit'
@@ -14,7 +17,13 @@ class ProfileController < ApplicationController
   private
 
     def profile_params
-      params.require(:user).permit(:name)
+      params.require(:user).permit(:name, :password, :password_confirmation)
+            .reject { |k,v| v.blank? }
+    end
+
+    def changing_password?
+      profile_params.include?(:password) &&
+      profile_params.include?(:password_confirmation)
     end
 
 end

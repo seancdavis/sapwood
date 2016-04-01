3a2d79b167b5a9aa1a6960795d17cf5ad6de55c2
Sean C Davis <scdavis41@gmail.com>
2016-04-01 11:52:39 -0400

    specs for hiding labels (fixes #2)

--- spec/features/properties/edit_property_spec.rb
+++ spec/features/properties/edit_property_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-feature 'Property Settings' do
+feature 'Property Settings', :js => true do
 
   background do
     @property = create(:property)
@@ -14,6 +14,24 @@ feature 'Property Settings' do
     fill_in 'property[title]', :with => new_title
     click_button 'Save Changes'
     expect(page).to have_content(new_title)
+  end
+
+  describe 'hiding sidebar labels' do
+    background { within('.properties') { click_link 'Edit' } }
+    scenario 'all should be shown/checked by default' do
+      expect(page).to have_css('input.label-visibility', :visible => false,
+                               :count => 5)
+    end
+    scenario 'toggling will add a hidden class to the parent' do
+      first('.label-input label').click
+      expect(page).to have_css('.label-input.hidden', :count => 1)
+    end
+    scenario 'will hide the sidebar item' do
+      expect(page).to have_content('Elements')
+      first('.label-input label').click
+      click_button 'Save Changes'
+      expect(page).to_not have_content('Elements')
+    end
   end
 
 end

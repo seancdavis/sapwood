eae47ae12172076af1f6b628baa3187c5ffb00c0
Sean C Davis <scdavis41@gmail.com>
2016-04-14 14:01:36 -0400

    trying to avoid the redirect on the installation (#18)

--- app/controllers/install_controller.rb
+++ app/controllers/install_controller.rb
@@ -8,7 +8,7 @@ class InstallController < ApplicationController
   helper_method :current_step
 
   def show
-    if params[:step].to_i == current_step
+    if params[:step].to_i <= current_step
       render current_step.to_s
     else
       redirect_to install_path(current_step)
@@ -34,7 +34,7 @@ class InstallController < ApplicationController
     def current_step
       @current_step ||= begin
         step = Sapwood.config.current_step
-        step = 1 if step.nil?
+        step = 1 if step.blank?
         Sapwood.set('current_step', step)
         Sapwood.write!
         step

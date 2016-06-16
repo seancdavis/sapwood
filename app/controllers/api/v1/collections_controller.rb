59b94f8c333e9ca6fc3a913ada6f11de81caf5f1
Sean C Davis <scdavis41@gmail.com>
2016-06-16 08:47:39 -0400

    add support for querying all collections

--- app/controllers/api/v1/collections_controller.rb
+++ app/controllers/api/v1/collections_controller.rb
@@ -1,5 +1,11 @@
 class Api::V1::CollectionsController < ApiController
 
+  def index
+    respond_to do |f|
+      f.json { render(:json => current_property.collections) }
+    end
+  end
+
   def show
     respond_to do |f|
       f.json do

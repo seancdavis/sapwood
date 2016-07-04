5dd596b95eb08b518b2522055b56bb503542432f
Sean C Davis <scdavis41@gmail.com>
2016-07-04 08:17:17 -0400

    add collection type name to json response of a collection (#59)

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -83,7 +83,7 @@ class Collection < ActiveRecord::Base
         items << e1.as_json({}).merge(:children => c1)
       end
     end
-    response = { :id => id, :title => title }
+    response = { :id => id, :title => title, :type => collection_type_name }
     if field_data.present?
       field_data.each do |k,v|
         response[k.to_sym] = if collection_type.find_field(k).is_document?

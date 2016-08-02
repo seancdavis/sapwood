3089e77e6e56f46061c09584a667587fc8077e7b
Sean C Davis <scdavis41@gmail.com>
2016-08-02 16:14:38 -0400

    temp duplicate routes for transition to nesting (#113)

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :properties, :only => [:show] do
-        resources :elements, :only => [:index, :show, :create]
+        resources :elements, :only => [:index, :show, :create] do
+          post 'webhook', :on => :collection if Rails.env.development?
+        end
+        resources :collections, :only => [:index, :show]
       end
       resources :elements, :only => [:index, :show] do
         post 'webhook', :on => :collection if Rails.env.development?

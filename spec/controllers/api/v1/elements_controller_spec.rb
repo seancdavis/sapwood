9ed6442a466c46d6b57d2b60c349761b0fd57649
Sean C Davis <scdavis41@gmail.com>
2016-08-02 16:13:56 -0400

    specs for create elements via API (#86)

--- spec/controllers/api/v1/elements_controller_spec.rb
+++ spec/controllers/api/v1/elements_controller_spec.rb
@@ -82,4 +82,44 @@ describe Api::V1::ElementsController do
     end
   end
 
+  # ---------------------------------------- Create
+
+  describe '#create' do
+    it 'raises 404 when no api_key' do
+      expect {
+        post :create, :property_id => @property.id, :format => :json
+      }.to raise_error(ActionController::RoutingError)
+    end
+    it 'raises 404 when api_key does not match property' do
+      expect {
+        post :create, :property_id => @property.id, :api_key => '123',
+             :format => :json
+      }.to raise_error(ActionController::RoutingError)
+    end
+    it 'raises 404 when template does not belong to site' do
+      expect {
+        post :create, :property_id => @property.id,
+             :api_key => @property.api_key, :format => :json,
+             :template => 'BAD TEMPLATE'
+      }.to raise_error(ActionController::RoutingError)
+    end
+    it 'will create an element and return the element as json' do
+      expect(@property.elements.count).to eq(0)
+      response = post :create, :property_id => @property.id,
+                      :api_key => @property.api_key, :format => :json,
+                      :name => (name = Faker::Lorem.words(4).join(' ')),
+                      :template => 'Default'
+      expect(response.body).to eq(@property.elements.first.to_json)
+      expect(@property.elements.count).to eq(1)
+      expect(@property.elements.first.title).to eq(name)
+    end
+    it 'returns validation errors if there are any' do
+      response = post :create, :property_id => @property.id,
+                      :api_key => @property.api_key, :format => :json,
+                      :name1 => 'BLAH BLAH', :template => 'Default'
+      expect(response.body)
+        .to eq({"title": ["can't be blank", "can't be blank"]}.to_json)
+    end
+  end
+
 end

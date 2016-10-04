ef9ba0a442bc8393f078b0b03898b36bc80a3cf6
Sean C Davis <scdavis41@gmail.com>
2016-10-04 11:45:36 -0400

    filter api collection calls by type (fixes #128)

--- spec/controllers/api/v1/collections_controller_spec.rb
+++ spec/controllers/api/v1/collections_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe Api::V1::CollectionsController do
 
   before(:each) do
-    @property = property_with_templates
+    @property = property_with_templates_and_collection_types
   end
 
   describe '#index' do
@@ -19,16 +19,35 @@ describe Api::V1::CollectionsController do
       expect { get :index, :property_id => @property.id, :api_key => '123' }
         .to raise_error(ActionController::RoutingError)
     end
-    it 'responds with the collections as json' do
-      c_01 = create(:collection, :with_items, :property => @property)
-      c_02 = create(:collection, :with_items, :property => @property)
-      c_03 = create(:collection, :with_items,
-                    :property => property_with_templates)
-      response = get :index, :property_id => @property.id,
-                     :api_key => @property.api_key, :format => :json
-      expect(response.body).to include(c_01.to_json)
-      expect(response.body).to include(c_02.to_json)
-      expect(response.body).to_not include(c_03.to_json)
+    context 'with collections' do
+      before(:each) do
+        @c_01 = create(:collection, :with_items, :property => @property)
+        @c_02 = create(:collection, :with_items, :with_options,
+                       :property => @property)
+        @c_03 = create(:collection, :with_items, :with_options,
+                       :property => property_with_templates_and_collection_types)
+      end
+      it 'responds with all property collections as json' do
+        response = get :index, :property_id => @property.id,
+                       :api_key => @property.api_key, :format => :json
+        expect(response.body).to include(@c_01.to_json)
+        expect(response.body).to include(@c_02.to_json)
+        expect(response.body).to_not include(@c_03.to_json)
+      end
+      it 'filters collections by type if specified' do
+        response = get :index, :property_id => @property.id,
+                       :api_key => @property.api_key, :format => :json,
+                       :type => 'Default Collection'
+        expect(response.body).to include(@c_01.to_json)
+        expect(response.body).to_not include(@c_02.to_json)
+        expect(response.body).to_not include(@c_03.to_json)
+      end
+      it 'returns no collections if the type does not exist' do
+        response = get :index, :property_id => @property.id,
+                       :api_key => @property.api_key, :format => :json,
+                       :type => 'WRONG! Collection'
+        expect(response.body).to eq('[]')
+      end
     end
   end
 

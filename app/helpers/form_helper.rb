32cc97fa029c2c50459e63ca4a57c8bf4f3a1855
Sean C Davis <scdavis41@gmail.com>
2016-12-18 15:48:10 -0500

    first quick pass at spec coverage (#147)

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -7,10 +7,4 @@ module FormHelper
                     &block)
   end
 
-  def minimal_fields_for(*args, &block)
-    options = args.extract_options!
-    simple_fields_for(*(args << options.merge(:builder => MinimalFormBuilder)),
-                      &block)
-  end
-
 end

99ab952de36e2b937256f3189b2292d1b2bda5cc
Sean C Davis <scdavis41@gmail.com>
2018-06-06 22:44:13 -0400

    [#252] Dynamic Errors

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,2 +1,18 @@
 class ErrorsController < ApplicationController
+
+  skip_before_action :authenticate_user!
+  skip_before_action :verify_profile_completion
+
+  def not_found
+    render status: 404
+  end
+
+  def server_error
+    render status: 500
+  end
+
+  def unacceptable
+    render status: 422
+  end
+
 end

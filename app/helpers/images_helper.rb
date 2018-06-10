954202235c6ccd2388b143e54d47096b0a8b3c36
Sean C Davis <scdavis41@gmail.com>
2018-06-10 17:24:47 -0400

    [#259] Fix specs

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -2,7 +2,7 @@ module ImagesHelper
 
   def image_thumb_url(document)
     return image_url('document.png') unless document.public? && document.image?
-    ix_image_url(document.path, auto: 'format,compress', w: 200, h: 200, fit: 'crop')
+    ix_image_url(document.path, auto: 'format,compress', w: 200, h: 200, fit: 'crop', sizes: '50px')
   end
 
 end

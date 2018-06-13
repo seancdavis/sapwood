1c08a4a18ff4fa8271de5e8849560e4f6fe79320
Sean C Davis <scdavis41@gmail.com>
2018-06-12 22:56:09 -0400

    [#264] Convert presenters to the heartwood decorator pattern

---
+++ app/decorators/element_decorator.rb
@@ -0,0 +1,13 @@
+module ElementDecorator
+
+  extend ActiveSupport::Concern
+
+  included do
+    # Instance methods go here ...
+  end
+
+  class_methods do
+    # Class methods go here ...
+  end
+
+end

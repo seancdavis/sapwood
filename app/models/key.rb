9dcd9bb0c7f381efd40812ed76b1d144330cbe56
Sean C Davis <scdavis41@gmail.com>
2018-06-14 07:30:35 -0400

    [#266] Add encryption and convert existing read keys

--- app/models/key.rb
+++ app/models/key.rb
@@ -4,4 +4,43 @@ class Key < ApplicationRecord
 
   belongs_to :property
 
+  # ---------------------------------------- | Validations
+
+  validates_presence_of :encrypted_value
+
+  # ---------------------------------------- | Callbacks
+
+  before_validation :set_encrypted_value
+
+  # ---------------------------------------- | Instance Methods
+
+  def value
+    @value ||= begin
+      return nil if encrypted_value.blank? || crypt.blank?
+      crypt.decrypt_and_verify(encrypted_value)
+    end
+  end
+
+  def value=(v)
+    @value = v
+  end
+
+  # ---------------------------------------- | Private Methods
+
+  private
+
+  def set_encrypted_value
+    return false if crypt.blank? || value.blank?
+    self.encrypted_value = crypt.encrypt_and_sign(value)
+  end
+
+  def crypt
+    @crypt ||= begin
+      return nil if ENV['API_ENCRYPTION_KEY'].blank? || ENV['API_ENCRYPTION_SALT'].blank?
+      key_gen = ActiveSupport::KeyGenerator.new(ENV['API_ENCRYPTION_KEY'])
+      key = key_gen.generate_key(ENV['API_ENCRYPTION_SALT'], 32)
+      crypt = ActiveSupport::MessageEncryptor.new(key)
+    end
+  end
+
 end
--- db/migrate/20180614104730_create_keys.rb
+++ db/migrate/20180614104730_create_keys.rb
@@ -1,5 +1,8 @@
+require 'pry'
+
 class CreateKeys < ActiveRecord::Migration[5.2]
-  def change
+
+  def up
     create_table :keys do |t|
       t.integer :property_id
       t.boolean :writeable, default: false
@@ -8,5 +11,16 @@ class CreateKeys < ActiveRecord::Migration[5.2]
 
       t.timestamps
     end
+
+    Property.all.each do |property|
+      Key.create!(property: property, value: property.api_key)
+    end
+
+    # remove_column :properties, :api_key
   end
+
+  def down
+    drop_table :keys
+  end
+
 end

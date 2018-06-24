d09ceb6a997b7d95102d10841d4499caaa7557f2
Sean C Davis <scdavis41@gmail.com>
2018-06-24 11:15:55 -0400

    [#269] Add pagination to search results

--- app/controllers/views_controller.rb
+++ app/controllers/views_controller.rb
@@ -3,6 +3,7 @@ class ViewsController < ApplicationController
   def show
     not_found if current_view.blank?
     @elements = SearchElementsService.call(property: current_property, q: current_view.q)
+    @elements = Kaminari.paginate_array(@elements).page(params[:page] || 1).per(20)
     render 'elements/search'
   end
 

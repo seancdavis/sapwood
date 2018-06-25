8d622736188353a5c80cd2854b5877ae8b81d681
Sean C Davis <scdavis41@gmail.com>
2018-06-25 07:23:36 -0400

    [#269] Can create views

--- app/controllers/views_controller.rb
+++ app/controllers/views_controller.rb
@@ -1,10 +1,35 @@
 class ViewsController < ApplicationController
 
+  before_action :verify_current_property
+
+  def new
+    @view = current_property.views.new(q: params[:q])
+  end
+
+  def create
+    @view = current_property.views.new(view_params)
+    if @view.save
+      redirect_to [current_property, @view], notice: 'View created successfully!'
+    else
+      render 'new'
+    end
+  end
+
   def show
     not_found if current_view.blank?
     @elements = SearchElementsService.call(property: current_property, q: current_view.q)
     @elements = Kaminari.paginate_array(@elements).page(params[:page] || 1).per(20)
     render 'elements/search'
   end
+
+  private
+
+    def verify_current_property
+      not_found unless current_property
+    end
+
+    def view_params
+      params.require(:view).permit(:title, :q)
+    end
 
 end
